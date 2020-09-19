8993b574cb1d77c5af85f33e23d20ad31fb29a63
99002656 <71486479+99002656@users.noreply.github.com>
2020-09-19 09:13:58 +0530

    Create money.h

---
+++ Implementation/inc/money.h
@@ -0,0 +1,16 @@
+
+#ifndef __MONEY_H__
+#define __MONEY_H__
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void validatePin(int PIN);
+int with_cash(int withdraw,int cash);
+int deposits_cash(int deposit,int cash);
+ int to_send(int send,int cash);
+
+
+
+
+#endif
